cf01f1164648dcf13e83f920360d4b14b1ca698d
joinmouse <908380434@qq.com>
2020-11-23 14:12:33 +0800

    init simple example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-/*
-    index: webpack入口文件
-*/
-import './index.css'
+import { hello } from './hello'
 
-function add(x, y) {
-    return x + y;
-}
-console.log('demo')
-export default add
+document.write(hello())
