c3a0318571c68a1a994fb48a4a3bc9ae1e620e32
joinmouse <908380434@qq.com>
2020-11-23 15:40:58 +0800

    解析图片和字体

--- src/search.js
+++ src/search.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './search.less'
+import x from './images/x.jpg'
 
 function Search() {
     return (
-        <div className="serach-text">Search Text....React</div>
+        <div className="serach-text">
+            Search Text....React
+            <img alt="x" src={x} />
+        </div>
     )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,21 @@ module.exports = {
                     'css-loader',
                     'less-loader'
                 ]
+            },
+            {
+                test: /.(png|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240   //小于10kb自动转为base64
+                        }
+                    }
+                ]
+            },
+            {
+                test: /.(woff|woff2|eot|ttf|otf)$/,
+                use: 'file-loader'
             }
         ]
     }
