4778f5c5219678884c4f3be051a108b490115b36
joinmouse <908380434@qq.com>
2020-11-23 15:05:13 +0800

    支持 react&es6 语法

--- src/search.js
+++ src/search.js
@@ -1,1 +1,13 @@
-document.write('search demo')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Search() {
+    return (
+        <div>Search Text</div>
+    )
+}
+
+ReactDOM.render(
+    <Search />,
+    document.querySelector('#app')
+)
